46b34559b69bd294456424743cffbb1aa85d351f
Milind Ganjoo <milind.ganjoo@gmail.com>
2017-02-03 13:09:20 -0800

    Update update script

--- update.sh
+++ update.sh
@@ -9,3 +9,6 @@ vim +PlugUpdate +qa
 
 # Remove plug.vim.old if it exists
 rm -f ~/.dotfiles/vim/autoload/plug.vim.old
+
+# Update fzf
+$HOME/.external/fzf/install --completion --key-bindings --no-update-rc
