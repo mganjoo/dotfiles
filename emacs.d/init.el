60a18c52866d685a64a528468ad51e0dabb77668
Milind Ganjoo <milind.ganjoo@gmail.com>
2017-05-28 14:35:03 -0700

    Reorganize init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,41 +5,58 @@
 
 ;;; Code:
 
+;; Don't clutter init.el file with custom stuff.
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file 'noerror)
 
-;; Boostrap package management.
+;; Package management.
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+             '("melpa" . "https://melpa.org/packages/")
+             '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (package-initialize)
-
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+(require 'use-package)
 (require 'diminish)
 
 ;; Basic configuration.
-(use-package better-defaults :ensure t)
-(setq inhibit-startup-screen t)
-(setq initial-scratch-message "")
-(setq vc-follow-symlinks t)
-;; Don't show warnings for redefined functions.
-(setq ad-redefinition-action 'accept)
+(setq
+ inhibit-startup-screen t
+ initial-scratch-message ""
+ column-number-mode t
+ make-backup-files nil
+ vc-follow-symlinks t
+ ad-redefinition-action 'accept ;; No warnings for redefined functions
+ use-package-always-ensure t)   ;; Avoid having to do ":ensure t" everywhere
+
+;; Some good defaults.
+(use-package better-defaults)
+
+;; Use PATH and other env variables from shell.
+(use-package exec-path-from-shell
+  :defer 1
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
 
 ;; Font.
 (add-to-list 'default-frame-alist '(font . "Fira Mono-14"))
 
+;; Theme.
+(use-package leuven-theme
+  :config
+  (load-theme 'leuven t))
+
 ;; Vimify.
 (use-package evil
-  :ensure t
   :config
 
   ;; Load `evil-leader' and enable mode before `evil-mode'
   ;; so it's available in every initial buffer.
   (use-package evil-leader
-    :ensure t
     :config
     (global-evil-leader-mode)
     (evil-leader/set-leader "<SPC>")
@@ -51,17 +68,14 @@
   (evil-mode 1)
 
   (use-package evil-surround
-    :ensure t
     :config
     (global-evil-surround-mode))
 
   (use-package evil-visualstar
-    :ensure t
     :config
     (global-evil-visualstar-mode))
 
   (use-package evil-commentary
-    :ensure t
     :config
     (evil-commentary-mode))
 
@@ -79,7 +93,6 @@
 
 ;; Helm.
 (use-package helm
-  :ensure t
   :diminish helm-mode
   :config
   (helm-mode 1)
@@ -88,7 +101,6 @@
   (global-set-key (kbd "M-x") 'helm-M-x)
 
   (use-package helm-descbinds
-    :ensure t
     :config
     (helm-descbinds-mode))
 
@@ -97,21 +109,14 @@
     (with-helm-buffer (setq cursor-in-non-selected-windows nil)))
   (add-hook 'helm-after-initialize-hook 'mg--remove-cursor-in-helm-buffers))
 
+;; Finding out what a specific binding is for.
 (use-package which-key
-  :ensure t
   :diminish which-key-mode
   :config
   (which-key-mode))
 
-;; Theme.
-(use-package leuven-theme
-  :ensure t
-  :config
-  (load-theme 'leuven t))
-
 ;; Code completion.
 (use-package company
-  :ensure t
   :diminish company-mode
   :defer 1
   :config
@@ -122,24 +127,18 @@
 
 ;; Error checking.
 (use-package flycheck
-  :ensure t
   :diminish flycheck-mode
   :config
   (add-hook 'after-init-hook 'global-flycheck-mode))
 
-(use-package exec-path-from-shell
-  :ensure t
-  :config
-  (when (memq window-system '(mac ns x))
-    (exec-path-from-shell-initialize)))
-
+;; Relative line numbering.
 (use-package nlinum-relative
-  :ensure t
   :config
   (nlinum-relative-setup-evil)
   (setq nlinum-relative-redisplay-delay 0)
   (add-hook 'prog-mode-hook 'nlinum-relative-mode))
 
+;; Some good defaults for prog-mode.
 (defun mg--set-up-prog-mode ()
   "Configure global `prog-mode' with basic defaults."
   (setq-local comment-auto-fill-only-comments t)
