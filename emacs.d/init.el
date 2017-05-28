f238b6573e7a98a269b0574c139a8180656b040d
Milind Ganjoo <milind.ganjoo@gmail.com>
2017-05-28 06:57:51 -0700

    Add initial version of init file
    - Add MELPA repo
    - Add `use-package'

---
+++ emacs.d/init.el
@@ -0,0 +1,10 @@
+;; Boostrap package management.
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
