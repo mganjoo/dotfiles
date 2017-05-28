2dd1bc6d093a4b9a45dd935e358c976c9fc6d986
Milind Ganjoo <milind.ganjoo@gmail.com>
2017-05-28 07:37:14 -0700

    Add evil

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,9 +6,35 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
+	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+(use-package evil
+  :ensure t
+  :config
+
+  ;; Load `evil-leader' and enable mode before `evil-mode'
+  ;; so it's available in every initial buffer.
+  (use-package evil-leader
+    :ensure t
+    :config
+    (global-evil-leader-mode)
+    (evil-leader/set-leader "<SPC>")
+    (evil-leader/set-key
+      ":" 'eval-expression))
+
+  (evil-mode 1)
+
+  (use-package evil-surround
+    :ensure t
+    :config
+    (global-evil-surround-mode))
+
+  (use-package evil-visualstar
+    :ensure t
+    :config
+    (global-evil-visualstar-mode)))
