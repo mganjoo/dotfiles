2b9417d5d152f2177b12e11b5fb23a8e9e19945c
Milind Ganjoo <milind.ganjoo@gmail.com>
2017-05-28 11:09:33 -0700

    Add more packages

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -18,9 +18,11 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+(require 'diminish)
 
 (use-package better-defaults :ensure t)
 
+;; Vimify.
 (use-package evil
   :ensure t
   :config
@@ -33,7 +35,8 @@
     (global-evil-leader-mode)
     (evil-leader/set-leader "<SPC>")
     (evil-leader/set-key
-      ":" 'eval-expression))
+      ":"     'eval-expression
+      "<SPC>" 'execute-extended-command))
 
   (evil-mode 1)
 
@@ -50,12 +53,50 @@
   (use-package evil-commentary
     :ensure t
     :config
-    (evil-commentary-mode)))
+    (evil-commentary-mode))
 
+  (dolist (mode '(flycheck-error-list-mode))
+    (add-to-list 'evil-emacs-state-modes mode))
+
+  ;; Bindings for other packages.
+  (add-hook 'flycheck-mode-hook
+            (lambda ()
+              (evil-define-key 'normal flycheck-mode-map
+                (kbd "]l") 'flycheck-next-error)
+              (evil-define-key 'normal flycheck-mode-map
+                (kbd "[l") 'flycheck-previous-error))))
+
+;; Helm.
+(use-package helm
+  :ensure t
+  :diminish helm-mode
+  :config
+  (helm-mode 1)
+  (define-key evil-ex-map "e" 'helm-find-files))
+
+;; Theme.
 (use-package leuven-theme
   :ensure t
   :config
   (load-theme 'leuven t))
+
+;; Code completion.
+(use-package company
+  :ensure t
+  :defer t
+  :config
+  (global-company-mode))
+
+;; Error checking.
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'after-init-hook 'global-flycheck-mode))
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
 
 (provide 'init)
 ;;; init.el ends here
