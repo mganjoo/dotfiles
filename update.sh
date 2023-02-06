6276cd25bdb57f2ec078b3cf6556a23af61a8017
Milind Ganjoo <milind.ganjoo@gmail.com>
2023-02-05 22:14:38 -0800

    Remove .external/fzf update call

--- update.sh
+++ update.sh
@@ -3,12 +3,3 @@
 # Update submodules
 git pull --recurse-submodules
 git submodule update --remote --recursive
-
-vim +PlugUpgrade +qa
-vim +PlugUpdate +qa
-
-# Remove plug.vim.old if it exists
-rm -f ~/.dotfiles/vim/autoload/plug.vim.old
-
-# Update fzf
-$HOME/.external/fzf/install --completion --key-bindings --no-update-rc
