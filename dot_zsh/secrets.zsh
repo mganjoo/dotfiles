c33b3324bacdfa03569823378cf2c2a64a2f96d4
milind.ganjoo@gmail.com <milind.ganjoo@gmail.com>
2024-06-03 20:40:18 -0700

    Update paths to secrets

--- dot_zsh/secrets.zsh
+++ dot_zsh/secrets.zsh
@@ -1,2 +1,2 @@
-export OPENAI_API_KEY="op://Private/OpenAI API Key/api key"
-export LANGCHAIN_API_KEY="op://Private/LangSmith API Key/credential"
+export OPENAI_API_KEY="op://Private/OpenAI/api key"
+export LANGCHAIN_API_KEY="op://Private/LangSmith/api key"
