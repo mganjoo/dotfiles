1008d0debe03ce08517ad0a4b458a5e792cdb232
milind.ganjoo@gmail.com <milind.ganjoo@gmail.com>
2025-06-22 22:44:08 -0700

    Clean up zsh configs

--- dot_zsh/interactive.zsh
+++ dot_zsh/interactive.zsh
@@ -10,6 +10,16 @@ fi
 
 fpath+=("$HOME/.zsh/external/zsh-completions/src")
 
+setopt COMPLETE_IN_WORD     # Complete from both ends of a word.
+setopt ALWAYS_TO_END        # Move cursor to the end of a completed word.
+setopt PATH_DIRS            # Perform path search even on command names with slashes.
+setopt AUTO_MENU            # Show completion menu on a successive tab press.
+setopt AUTO_LIST            # Automatically list choices on ambiguous completion.
+setopt AUTO_PARAM_SLASH     # If completed parameter is a directory, add a trailing slash.
+setopt EXTENDED_GLOB        # Needed for file modification glob modifiers with compinit.
+unsetopt MENU_COMPLETE      # Do not autoselect the first completion entry.
+unsetopt FLOW_CONTROL       # Disable start/stop characters in shell editor.
+
 if [[ -d "/opt/homebrew/share/zsh/site-functions" ]]; then
   fpath+=("/opt/homebrew/share/zsh/site-functions")
 fi
@@ -36,7 +46,6 @@ zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
 zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
 zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
 zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
-zstyle ':completion:*:default' list-prompt '%S%M matches%s'
 zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' verbose yes
