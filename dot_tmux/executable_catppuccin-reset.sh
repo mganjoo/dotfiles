190fe7e5467ff14de6555a54edbdf0789d4b2f99
milind.ganjoo@gmail.com <milind.ganjoo@gmail.com>
2025-06-27 22:19:34 -0700

    Add .tmux/catppuccin-reset.sh

---
+++ dot_tmux/executable_catppuccin-reset.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+CATPPUCCIN_PLUGIN_DIR="$HOME/.tmux/plugins/catppuccin"
+
+# Unset all catppuccin options (tmux options that begin with @)
+rg -Io 'set\s+-[aFgopqsuUw]+\s+"?@([^\s]+(\w|_))"?' -r '@$1' $CATPPUCCIN_PLUGIN_DIR/**/*.conf | uniq | xargs -n1 -P0 tmux set -Ugq
+
+status_dir="$CATPPUCCIN_PLUGIN_DIR/status"
+status_utils_file="$CATPPUCCIN_PLUGIN_DIR/utils/status_module.conf"
+
+# Get all status modules in status_dir
+modules=()
+for filepath in "$status_dir"/*.conf; do
+  [ -e "$filepath" ] || continue
+  filename="$(basename "$filepath" .conf)"
+  modules+=("$filename")
+done
+
+# Unset status module options for each module
+for module in "${modules[@]}"; do
+  conf_file="${status_dir}/${module}.conf"
+
+  rg -Io 'set\s+-[aFgopqsuUw]+\s+"?@([^\s]+(\w|_))"?' -r '@$1' "$conf_file" | sed "s/\${MODULE_NAME}/$module/g" | uniq | xargs -n1 -P0 tmux set -Ugq
+  rg -Io 'set\s+-[aFgopqsuUw]+\s+"?@([^\s]+(\w|_))"?' -r '@$1' "$status_utils_file" | sed "s/\${MODULE_NAME}/$module/g" | uniq | xargs -n1 -P0 tmux set -Ugq
+done
