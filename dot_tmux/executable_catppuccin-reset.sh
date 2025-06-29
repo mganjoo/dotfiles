7bd6fc5913b43378fbfc52e6d2e8a4f8e590c130
milind.ganjoo@gmail.com <milind.ganjoo@gmail.com>
2025-06-28 23:10:08 -0700

    Unset tmux options from within status modules too

--- dot_tmux/executable_catppuccin-reset.sh
+++ dot_tmux/executable_catppuccin-reset.sh
@@ -8,12 +8,15 @@ CATPPUCCIN_PLUGIN_DIR="$HOME/.tmux/catppuccin"
 status_dir="$CATPPUCCIN_PLUGIN_DIR/status"
 status_utils_file="$CATPPUCCIN_PLUGIN_DIR/utils/status_module.conf"
 
-# Unset all theme options (beginning with '@') so we can set them again
-unset_theme_options() {
+# Custom status modules that are not part of base cappuccin distribution
+custom_status_dir="$HOME/.tmux/status"
+
+# Collect all theme options (beginning with '@') to unset
+read_theme_options() {
   local file_pattern="$1"
   local module_name="${2:-}"
 
-  rg -Io 'set\s+-[aFgopqsuUw]+\s+"?@([^\s]+(\w|_))"?' -r '@$1' \
+  rg -Io 'set\s+-[aFgopqsuUw]+\s+"?@([^\s]+(\w|_|\}))"?' -r '@$1' \
     $file_pattern | while IFS= read -r line; do
     # If module_name is passed in, replace it in matched lines, else skip
     if [[ "$line" == *'${MODULE_NAME}'* ]]; then
@@ -23,23 +26,36 @@ unset_theme_options() {
     else
       echo "$line"
     fi
-  done | uniq | xargs -n1 -P0 tmux set -Ugq
+  done
 }
 
-unset_theme_options "$CATPPUCCIN_PLUGIN_DIR/**/*.conf"
+collect_theme_options() {
+  while IFS= read -r option; do
+    all_options+=("$option")
+  done < <(read_theme_options "$@")
+}
 
-# Get all status modules in status_dir
-modules=()
-for filepath in "$status_dir"/*.conf; do
+# Collect all options to unset
+all_options=()
+
+# Get options from all conf files
+collect_theme_options "$CATPPUCCIN_PLUGIN_DIR/**/*.conf"
+
+# Get all status modules in status_dir and custom_status_dir
+filepaths=()
+for filepath in "$status_dir"/*.conf "$custom_status_dir"/*.conf; do
   [ -e "$filepath" ] || continue
-  filename="$(basename "$filepath" .conf)"
-  modules+=("$filename")
+  filepaths+=("$filepath")
 done
 
-# Unset status module options for each module
-for module in "${modules[@]}"; do
-  conf_file="${status_dir}/${module}.conf"
+# Collect options from status module files
+for conf_file in "${filepaths[@]}"; do
+  module="$(basename "$conf_file" .conf)"
 
-  unset_theme_options "$conf_file" "$module"
-  unset_theme_options "$status_utils_file" "$module"
+  collect_theme_options "$conf_file" "$module"
+  collect_theme_options "$status_utils_file" "$module"
 done
+
+# Unset all collected options
+printf '%s\n' "${all_options[@]}" | sort -u | \
+  xargs -n1 -P0 tmux set -Ugq
