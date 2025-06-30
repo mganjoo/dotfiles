9008a78807101a380c944c1ef65b6d88ac9698a6
milind.ganjoo@gmail.com <milind.ganjoo@gmail.com>
2025-06-30 09:14:31 -0700

    A few more modernizations

--- dot_zsh/fzf.zsh
+++ dot_zsh/fzf.zsh
@@ -1,16 +1,19 @@
-export FZF_DEFAULT_COMMAND='(fd --type f --strip-cwd-prefix --hidden --follow --exclude .git) 2>/dev/null'
-export FZF_CTRL_T_COMMAND='(fd --type f --strip-cwd-prefix --hidden --follow --exclude .git) 2>/dev/null'
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+if (( $+commands[fzf] )); then
+  source <(fzf --zsh)
 
-# Branch widget for FZF.
-function __bsel() {
-  git for-each-ref --format='%(refname:short)' refs/heads/ 2>/dev/null | fzf -m | tr "\n" " "
-}
-function fzf-branch-widget() {
-  LBUFFER="${LBUFFER}$(__bsel)"
-  zle redisplay
-}
-zle -N fzf-branch-widget
+  export FZF_DEFAULT_COMMAND='(fd --type f --strip-cwd-prefix --hidden --follow --exclude .git) 2>/dev/null'
+  export FZF_CTRL_T_COMMAND='(fd --type f --strip-cwd-prefix --hidden --follow --exclude .git) 2>/dev/null'
 
-# Paste the selected branches into the command line.
-bindkey -M viins '^b' fzf-branch-widget
+  # Branch widget for FZF.
+  function __bsel() {
+    git for-each-ref --format='%(refname:short)' refs/heads/ 2>/dev/null | fzf -m | tr "\n" " "
+  }
+  function fzf-branch-widget() {
+    LBUFFER="${LBUFFER}$(__bsel)"
+    zle redisplay
+  }
+  zle -N fzf-branch-widget
+
+  # Paste the selected branches into the command line.
+  bindkey -M viins '^b' fzf-branch-widget
+fi
