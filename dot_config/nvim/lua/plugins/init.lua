9cad62eac11e44890c8741a8de6c74c9531cb35f
Milind Ganjoo <milind.ganjoo@gmail.com>
2025-07-01 13:37:45 -0700

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/init.lua

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -127,4 +127,16 @@ return {
     config = true,
     dependencies = 'nvim-tree/nvim-web-devicons',
   },
+  {
+    'mason-org/mason-lspconfig.nvim',
+    dependencies = {
+      { 'mason-org/mason.nvim', config = true },
+      'neovim/nvim-lspconfig',
+    },
+    opts = {
+      ensure_installed = {
+        'lua_ls',
+      }
+    },
+  },
 }
