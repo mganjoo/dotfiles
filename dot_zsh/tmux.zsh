08a5a61671da43fb3ea505ffe8a02aaed05fbdc7
Milind Ganjoo <milind.ganjoo@gmail.com>
2025-07-10 22:33:10 -0700

    Add .zsh/tmux.zsh

---
+++ dot_zsh/tmux.zsh
@@ -0,0 +1,9 @@
+# Aliases
+alias ta="tmux attach"
+
+# Create or attach to a session named "username" by default
+if [[ -x "$(command -v tmux)" ]] && \
+  [[ -z "${TMUX}" ]] && \
+  [[ "${TERM_PROGRAM}" == *"iTerm"* ]]; then
+    exec tmux new-session -A -s ${USER} >/dev/null 2>&1
+fi
