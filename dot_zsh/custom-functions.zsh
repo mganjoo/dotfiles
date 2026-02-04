4ec8b32925d92f45489c23c0b796fc1db034be60
Milind Ganjoo <milind@resolve.ai>
2026-02-04 13:39:11 -0800

    Add in_dir alias

--- dot_zsh/custom-functions.zsh
+++ dot_zsh/custom-functions.zsh
@@ -6,6 +6,10 @@ function is-true {
   [[ -n "$1" && "$1" == (1|[Yy]([Ee][Ss]|)|[Tt]([Rr][Uu][Ee]|)|[Oo]([Nn]|)) ]]
 }
 
+function in_dir {
+  pushd "$1" > /dev/null && eval "${@:2}" && popd > /dev/null
+}
+
 # Autoload all functions in the directory
 # (exclude dotfiles and temp files)
 local function_glob='^([.]*|*~)(-.N:t)'
